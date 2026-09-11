b68b49d74368122e3541ce698469125f47bc8499
Arab45 <horlakunleolayiwola@gmail.com>
2026-09-11 15:01:06 -0700

    expenses tracker

--- src/ExpensesTracker.java
+++ src/ExpensesTracker.java
@@ -1,21 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ExpensesTracker {
     String fullName;
     double balance;
-    double expenses;
+    double[] expenses = new double[3];
+    boolean isTrue = false;
     public void dailyTrackerExpenses(){
         Scanner myData = new Scanner(System.in);
-        System.out.println("Enter Your Full Name");
+        System.out.println("Enter Your Full Name: ");
         fullName = myData.nextLine();
         System.out.println("My name is: " + fullName);
 
-        System.out.println("Enter Your Balance");
+        System.out.println("Enter Your Balance: ");
         balance = myData.nextDouble();
         System.out.println("Amount: " + balance);
 
-        System.out.println("Enter Your Daily Expenses");
-        expenses = myData.nextDouble();
-        System.out.println("Amount: " + expenses);
+        for(int i = 0; i < expenses.length; i++) {
+            System.out.println("Enter Your Daily Expenses: ");
+            double expense = myData.nextDouble();
+            Arrays.fill(expenses, expense);
+            System.out.println(TrackerStatus.DEBIT + " " + expense + " " + "in" + " " + balance + " " + "balance");
+            balance -= expense;
+            System.out.println("Balance: " + balance);
+            if(balance < 0){
+                System.out.println("Insufficient account balance" + " " + balance);
+            }
+
+//            isTrue = true;
+//            switch (isTrue){
+//                case expenses[i] > balance:
+//                    System.out.println("");
+//            }
+        }
     }
 }
--- src/TrackerStatus.java
+++ src/TrackerStatus.java
@@ -1,2 +1,6 @@
 public enum TrackerStatus {
+    PENDING,
+    PAID,
+    WIDTHRAW,
+    DEBIT
 }
